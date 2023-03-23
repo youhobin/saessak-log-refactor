379648c34a79bcefdfb97f2269b412f5a284c5a7
youhobin <ghdb132@naver.com>
2023-03-23 14:42:33 +0900

    test : PostServiceTest
    PostService test 코드 작성

--- src/test/java/saessak/log/post/service/PostServiceTest.java
+++ src/test/java/saessak/log/post/service/PostServiceTest.java
@@ -10,12 +10,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import saessak.log.post.Post;
-import saessak.log.post.dto.PostResponseDto;
+import saessak.log.post.dto.*;
 import saessak.log.post.repository.PostRepository;
 import saessak.log.post_media.PostMedia;
+import saessak.log.reaction.Reaction;
+import saessak.log.reaction.repository.ReactionRepository;
+import saessak.log.subscription.Subscription;
+import saessak.log.subscription.repository.SubscriptionRepository;
 import saessak.log.user.User;
 import saessak.log.user.dto.UserJoinDto;
 import saessak.log.user.repository.UserRepository;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,6 +34,8 @@ public class PostServiceTest {
     @Autowired PostService postService;
     @Autowired UserRepository userRepository;
     @Autowired BCryptPasswordEncoder encoder;
+    @Autowired ReactionRepository reactionRepository;
+    @Autowired SubscriptionRepository subscriptionRepository;
 
     static User user;
     static Post post;
@@ -39,9 +47,21 @@ public class PostServiceTest {
         user = user1.passwordEncode(encoder);
         userRepository.save(user);
 
-        PostMedia postMedia = PostMedia.of("imageFile", "text");
+        PostMedia postMedia = PostMedia.of("image1", "text1");
         post = Post.of(user, postMedia);
         postRepository.save(post);
+
+        PostMedia postMedia2 = PostMedia.of("image2", "text2");
+        Post post2 = Post.of(user, postMedia2);
+        postRepository.save(post2);
+
+        PostMedia postMedia3 = PostMedia.of("image3", "text3");
+        Post post3 = Post.of(user, postMedia3);
+        postRepository.save(post3);
+
+        Reaction reaction = Reaction.of(user, post);
+        reactionRepository.save(reaction);
+        post.plusReactionCount();
     }
 
     @Test
@@ -53,10 +73,106 @@ public class PostServiceTest {
         //then
         assertThat(postResponseDto.getPostText()).isEqualTo(post.getPostMedia().getPostText());
         assertThat(postResponseDto.getProfileId()).isEqualTo(user.getProfileId());
-        assertThat(postResponseDto.getLikeCount()).isEqualTo(0);
+        assertThat(postResponseDto.getLikeCount()).isEqualTo(1);
         assertThat(postResponseDto.getImageFile())
             .isEqualTo("https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/" + post.getPostMedia().getImageFile());
+        assertThat(postResponseDto.isLike()).isFalse();
+        assertThat(postResponseDto.isSubscribe()).isFalse();
     }
 
+    @Test
+    @DisplayName("게시글 단건 조회 - 로그인 O")
+    public void findPost_Login() throws Exception {
+        //when
+        PostResponseDto postResponseDto = postService.findPost(post.getId(), user.getProfileId());
 
+        //then
+        assertThat(postResponseDto.getPostText()).isEqualTo(post.getPostMedia().getPostText());
+        assertThat(postResponseDto.getProfileId()).isEqualTo(user.getProfileId());
+        assertThat(postResponseDto.getLikeCount()).isEqualTo(1);
+        assertThat(postResponseDto.getImageFile())
+            .isEqualTo("https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/" + post.getPostMedia().getImageFile());
+        assertThat(postResponseDto.isLike()).isTrue();
+    }
+
+    @Test
+    @DisplayName("메인 페이지 - 게시글 전체조회 - 좋아요 갯수 정렬")
+    public void findAllPostsByLikeCount() throws Exception {
+        //given
+        int limit = 2;
+        int page = 0;
+
+        //when
+        PostAllResponseDto postAllResponseDto = postService.findAllPostsByLikeCount(limit, page);
+        List<PostMainDto> postMainDtoList = postAllResponseDto.getPostMainDtoList();
+        PostMainDto post1 = postMainDtoList.get(0);
+        PostMainDto post2 = postMainDtoList.get(1);
+
+        //then
+        assertThat(postMainDtoList.size()).isEqualTo(2);
+        assertThat(post1.getProfileId()).isEqualTo("ghdb132");
+        assertThat(post1.getReactionCount()).isEqualTo(1);
+        assertThat(post1.getImageFile())
+            .isEqualTo("https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/image1");
+
+        assertThat(post2.getProfileId()).isEqualTo("ghdb132");
+        assertThat(post2.getReactionCount()).isEqualTo(0);
+        assertThat(post2.getImageFile())
+            .isEqualTo("https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/image3");
+    }
+
+    @Test
+    @DisplayName("내 활동")
+    public void myActivity() throws Exception {
+        //given
+        int page = 0;
+        int limit = 2;
+
+        //when
+        MyActivitiesResponse myActivity = postService.getMyActivity("ghdb132", page, limit);
+        List<PostMyActivityDto> postMyActivityDtoList = myActivity.getPostMyActivityDtoList();
+        PostMyActivityDto recentMyActivity1 = postMyActivityDtoList.get(0);
+        PostMyActivityDto recentMyActivity2 = postMyActivityDtoList.get(1);
+
+
+        //then
+        assertThat(recentMyActivity1.getImageFile())
+            .isEqualTo("https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/image3");
+        assertThat(recentMyActivity1.getReactionCount())
+            .isEqualTo(0);
+
+        assertThat(recentMyActivity2.getImageFile())
+            .isEqualTo("https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/image2");
+        assertThat(recentMyActivity2.getReactionCount())
+            .isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("구독한 사람의 게시물 가져오기")
+    public void subscribePost() throws Exception {
+        //given
+        UserJoinDto userJoinDto1 = new UserJoinDto("user2", "1234", "1234", "erer", "user2@naver.com");
+        User user2 = userJoinDto1.toEntity();
+        user2.passwordEncode(encoder);
+        userRepository.save(user2);
+
+        Subscription subscription = Subscription.of(user, user2);
+        subscriptionRepository.save(subscription);
+
+        //when
+        SubscribePostResponse subscribePostResponse = postService.getSubscribedPosts("user2", 0, 3);
+        List<SubscribePostDto> subscribePostList = subscribePostResponse.getSubscribePostList();
+        SubscribePostDto post3 = subscribePostList.get(0);
+        SubscribePostDto post1 = subscribePostList.get(2);
+
+        //then
+        assertThat(post3.getProfileId()).isEqualTo("ghdb132");
+        assertThat(post3.getReactionCount()).isEqualTo(0);
+        assertThat(post3.getImageFile())
+            .isEqualTo("https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/image3");
+
+        assertThat(post1.getReactionCount()).isEqualTo(1);
+        assertThat(post1.getImageFile())
+            .isEqualTo("https://saessaklogfile.s3.ap-northeast-2.amazonaws.com/image/image1");
+    }
 }
